916bed83b4ae0f5dec89b05ef4183e28229b18c1
TronTheGamer <yadu1raj@gmail.com>
2022-03-01 18:21:50 +0530

    readme

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   return runApp(MyApp());
@@ -32,18 +33,27 @@ class DicePage extends StatelessWidget {
             // flex: 1,
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Image.asset("images/dice1.png"),
+              child: FlatButton(child: Image.asset("images/dice1.png"),
+                onPressed: (){print("Pressed left btn");},
+              ),
             ),
           ),
           Expanded(
             // flex: 1,
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Image.asset("images/dice2.png"),
+              child: FlatButton(child: Image.asset("images/dice2.png"),
+                onPressed: (){Fluttertoast.showToast(
+                  msg: "Pressed Right btn",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                );},),
             ),
           ),
         ],
       ),
     );
   }
+
 }
+
--- readme.md
+++ readme.md
@@ -1,5 +1,31 @@
-# Key Bindings (*Android Studio*) <kbd>&#x2328;</kbd>
+# **FLUTTER**
+
+# &#x2328; Key Bindings (*Android Studio*)
 
 * <kbd>Alt/&#x2325;</kbd> + <kbd>&#x23CE;</kbd> : ***Wrap Widget into Common Widgets***
 
 * <kbd>Ctrl / &#x2318;</kbd> + <kbd>s</kbd> : ***Hot Reload***
+
+
+# &#x1f4b8; Tips
+
+* Always check flutter installation on a new device:
+    ```bash
+    flutter doctor -v
+    ```
+* To ensure everything is working, 
+  * create a new empty project:
+    ```
+    flutter create <project-name>
+    ```
+  * Then go into the ***\<project-name\>*** directory in Android Studio or VS code and run the project **main.dart** file.
+
+# &#x2699; Setting up Flutter
+
+* Follow [Flutter official guide](https://docs.flutter.dev/get-started/install).
+
+* [VS - Code Setup](https://docs.flutter.dev/get-started/editor?tab=vscode)
+  
+* [&#x1F916; Android Studio](https://docs.flutter.dev/get-started/editor?tab=androidstudio)
+
+* [Emacs!](https://docs.flutter.dev/get-started/editor?tab=emacs)
